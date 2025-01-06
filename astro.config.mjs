6f756d6935d1839567cdeb2c514d904defa5c493
enzo-buranello <enzoburanelloo@gmail.com>
2025-01-06 12:31:59 +0100

    css-test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://enzo-buranello.github.io/enzo.portfolio/',
-    integrations: [mdx(), sitemap(), react(), tailwind()],
+  site: 'https://enzo-buranello.github.io/enzo.portfolio/',
+  base: '/enzo.portfolio/', // Ajoutez cette ligne
+  integrations: [mdx(), sitemap(), react(), tailwind()],
 });
