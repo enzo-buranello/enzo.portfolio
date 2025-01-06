bb286871a571bd127f61aa035ae5e5304838e83a
enzo-buranello <enzoburanelloo@gmail.com>
2025-01-06 12:16:35 +0100

    css

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://enzo-buranello.github.io/enzo.portfolio/',
     integrations: [mdx(), sitemap(), react(), tailwind()],
 });
